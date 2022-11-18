8bcc0801220bef380f59c707e447cc8a3e8abdd3
Pug-King-Dean <94318068+Pug-King-Dean@users.noreply.github.com>
2022-11-18 13:23:34 +0000

    Add files via upload

---
+++ PartyPlaza/PartyPlaza/FrmCustomer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PartyPlaza
+{
+    public partial class FrmCustomer : Form
+    {
+        SqlDataAdapter daCustomer;
+        DataSet partyPlaza = new DataSet();
+        SqlCommandBuilder cmdBCustomer;
+        DataRow drCustomer;
+        String connStr, sqlCustomer;
+        public FrmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCustomer_Load(object sender, EventArgs e)
+        {
+
+        connStr = @"Data Source = 286386-PC\SQLEXPRESS01; Initial Catalog = PartyPlaza; Integrated Security = true";
+
+            sqlCustomer = @"select * from Customer";
+            daCustomer = new SqlDataAdapter(sqlCustomer, connStr);
+            cmdBCustomer = new SqlCommandBuilder(daCustomer);
+            daCustomer.FillSchema(partyPlaza, SchemaType.Source, "Customer");
+            daCustomer.Fill(partyPlaza, "Customer");
+
+            dgvCustomer.DataSource = partyPlaza.Tables["Customer"];
+            dgvCustomer.AutoResizeColumns((DataGridViewAutoSizeColumnsMode)DataGridViewAutoSizeColumnMode.AllCells);
+        }
+
+      
+        private void dgvCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void butExitCust_Click(object sender, EventArgs e)
+        {
+            MyGlobals.frmClosing = true;
+            Close();
+        }
+
+        private void btnDeleteDisplay_Click(object sender, EventArgs e)
+        {
+            if (dgvCustomer.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a customer from the list.", "Select Customer");
+            }
+            else
+            {
+                drCustomer = partyPlaza.Tables["Customer"].Rows.Find(dgvCustomer.SelectedRows[0].Cells[0].Value);
+                string tempName = drCustomer["Forename"].ToString() + " " + drCustomer["Surname"].ToString();
+
+                if (MessageBox.Show("Are you sure you want to delete " + tempName + " details? ", "Add Customer", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    drCustomer.Delete();
+                    daCustomer.Update(partyPlaza, "Customer");
+                }
+            }
+        }
+    }
+}
---
+++ PartyPlaza/PartyPlaza/FrmEditCustomer.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace PartyPlaza
+{
+    partial class FrmEditCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlEditCustomer = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labCustomerNumEdit = new System.Windows.Forms.Label();
+            this.txtEditSurname = new System.Windows.Forms.TextBox();
+            this.txtEditForename = new System.Windows.Forms.TextBox();
+            this.txtEditStreet = new System.Windows.Forms.TextBox();
+            this.txtEditTown = new System.Windows.Forms.TextBox();
+            this.txtEditCounty = new System.Windows.Forms.TextBox();
+            this.txtEditPostcode = new System.Windows.Forms.TextBox();
+            this.txtEditTelNo = new System.Windows.Forms.TextBox();
+            this.txtEditEmail = new System.Windows.Forms.TextBox();
+            this.btnEditEdit = new System.Windows.Forms.Button();
+            this.BtnEditCancel = new System.Windows.Forms.Button();
+            this.pnlEditCustomer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlEditCustomer
+            // 
+            this.pnlEditCustomer.Controls.Add(this.BtnEditCancel);
+            this.pnlEditCustomer.Controls.Add(this.btnEditEdit);
+            this.pnlEditCustomer.Controls.Add(this.txtEditEmail);
+            this.pnlEditCustomer.Controls.Add(this.txtEditTelNo);
+            this.pnlEditCustomer.Controls.Add(this.txtEditPostcode);
+            this.pnlEditCustomer.Controls.Add(this.txtEditCounty);
+            this.pnlEditCustomer.Controls.Add(this.txtEditTown);
+            this.pnlEditCustomer.Controls.Add(this.txtEditStreet);
+            this.pnlEditCustomer.Controls.Add(this.txtEditForename);
+            this.pnlEditCustomer.Controls.Add(this.txtEditSurname);
+            this.pnlEditCustomer.Controls.Add(this.labCustomerNumEdit);
+            this.pnlEditCustomer.Controls.Add(this.label9);
+            this.pnlEditCustomer.Controls.Add(this.label8);
+            this.pnlEditCustomer.Controls.Add(this.label7);
+            this.pnlEditCustomer.Controls.Add(this.label6);
+            this.pnlEditCustomer.Controls.Add(this.label5);
+            this.pnlEditCustomer.Controls.Add(this.label4);
+            this.pnlEditCustomer.Controls.Add(this.label3);
+            this.pnlEditCustomer.Controls.Add(this.label2);
+            this.pnlEditCustomer.Controls.Add(this.label1);
+            this.pnlEditCustomer.Location = new System.Drawing.Point(60, 2);
+            this.pnlEditCustomer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlEditCustomer.Name = "pnlEditCustomer";
+            this.pnlEditCustomer.Size = new System.Drawing.Size(836, 388);
+            this.pnlEditCustomer.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(24, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(24, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Forename";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(24, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Town";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(24, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "County";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(24, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Street";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(24, 342);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(24, 305);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Telephone Number";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(24, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 19);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Postcode";
+            // 
+            // labCustomerNumEdit
+            // 
+            this.labCustomerNumEdit.AutoSize = true;
+            this.labCustomerNumEdit.Location = new System.Drawing.Point(190, 20);
+            this.labCustomerNumEdit.Name = "labCustomerNumEdit";
+            this.labCustomerNumEdit.Size = new System.Drawing.Size(12, 15);
+            this.labCustomerNumEdit.TabIndex = 9;
+            this.labCustomerNumEdit.Text = "-";
+            // 
+            // txtEditSurname
+            // 
+            this.txtEditSurname.Location = new System.Drawing.Point(190, 56);
+            this.txtEditSurname.Name = "txtEditSurname";
+            this.txtEditSurname.Size = new System.Drawing.Size(241, 23);
+            this.txtEditSurname.TabIndex = 10;
+            // 
+            // txtEditForename
+            // 
+            this.txtEditForename.Location = new System.Drawing.Point(190, 96);
+            this.txtEditForename.Name = "txtEditForename";
+            this.txtEditForename.Size = new System.Drawing.Size(241, 23);
+            this.txtEditForename.TabIndex = 11;
+            // 
+            // txtEditStreet
+            // 
+            this.txtEditStreet.Location = new System.Drawing.Point(190, 138);
+            this.txtEditStreet.Name = "txtEditStreet";
+            this.txtEditStreet.Size = new System.Drawing.Size(241, 23);
+            this.txtEditStreet.TabIndex = 12;
+            // 
+            // txtEditTown
+            // 
+            this.txtEditTown.Location = new System.Drawing.Point(190, 179);
+            this.txtEditTown.Name = "txtEditTown";
+            this.txtEditTown.Size = new System.Drawing.Size(241, 23);
+            this.txtEditTown.TabIndex = 13;
+            // 
+            // txtEditCounty
+            // 
+            this.txtEditCounty.Location = new System.Drawing.Point(190, 225);
+            this.txtEditCounty.Name = "txtEditCounty";
+            this.txtEditCounty.Size = new System.Drawing.Size(241, 23);
+            this.txtEditCounty.TabIndex = 14;
+            // 
+            // txtEditPostcode
+            // 
+            this.txtEditPostcode.Location = new System.Drawing.Point(190, 267);
+            this.txtEditPostcode.Name = "txtEditPostcode";
+            this.txtEditPostcode.Size = new System.Drawing.Size(241, 23);
+            this.txtEditPostcode.TabIndex = 15;
+            // 
+            // txtEditTelNo
+            // 
+            this.txtEditTelNo.Location = new System.Drawing.Point(190, 305);
+            this.txtEditTelNo.Name = "txtEditTelNo";
+            this.txtEditTelNo.Size = new System.Drawing.Size(241, 23);
+            this.txtEditTelNo.TabIndex = 16;
+            // 
+            // txtEditEmail
+            // 
+            this.txtEditEmail.Location = new System.Drawing.Point(190, 342);
+            this.txtEditEmail.Name = "txtEditEmail";
+            this.txtEditEmail.Size = new System.Drawing.Size(241, 23);
+            this.txtEditEmail.TabIndex = 17;
+            // 
+            // btnEditEdit
+            // 
+            this.btnEditEdit.Location = new System.Drawing.Point(552, 57);
+            this.btnEditEdit.Name = "btnEditEdit";
+            this.btnEditEdit.Size = new System.Drawing.Size(256, 47);
+            this.btnEditEdit.TabIndex = 18;
+            this.btnEditEdit.Text = "Edit";
+            this.btnEditEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnEditCancel
+            // 
+            this.BtnEditCancel.Location = new System.Drawing.Point(552, 138);
+            this.BtnEditCancel.Name = "BtnEditCancel";
+            this.BtnEditCancel.Size = new System.Drawing.Size(256, 46);
+            this.BtnEditCancel.TabIndex = 19;
+            this.BtnEditCancel.Text = "Cancel";
+            this.BtnEditCancel.UseVisualStyleBackColor = true;
+            // 
+            // FrmEditCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 386);
+            this.Controls.Add(this.pnlEditCustomer);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FrmEditCustomer";
+            this.Text = "FrmEditCustomer1";
+            this.Load += new System.EventHandler(this.FrmEditCustomer_Load);
+            this.pnlEditCustomer.ResumeLayout(false);
+            this.pnlEditCustomer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel pnlEditCustomer;
+        private Label labCustomerNumEdit;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button BtnEditCancel;
+        private Button btnEditEdit;
+        private TextBox txtEditEmail;
+        private TextBox txtEditTelNo;
+        private TextBox txtEditPostcode;
+        private TextBox txtEditCounty;
+        private TextBox txtEditTown;
+        private TextBox txtEditStreet;
+        private TextBox txtEditForename;
+        private TextBox txtEditSurname;
+    }
+}
